9d91e01ae710db97cfe119877745b0ac9805b3f4
kk0walski <karol.kowalski95@gmail.com>
2021-10-13 23:35:38 +0200

    Add models to admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,12 +3,61 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Address, Participant
+
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ['email', 'username', ]
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    model = Participant
+    list_display = ("user", "nationality", "birthday")
+    list_filter = ("user", "nationality", "birthday")
+    fieldsets = (
+        (_("Contact info"), {
+         "fields": ("user", "phone_number", "cellphone_number")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "birthday",
+                    "site",
+                    "nationality",
+                )
+            },
+        ),
+    )
+    search_fields = ("user",)
+    ordering = ("user", "nationality")
+
+
+admin.site.register(Participant, ParticipantAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_filter = ("user", "country", "city", "zip_code")
+    list_display = ("fullAddress", "zip_code", "city", "country")
+    fieldsets = (
+        (_("Basic info"), {
+         "fields": ("user", "fullAddress", "country", "city")}),
+        (
+            _("Address info"),
+            {
+                "fields": (
+                    "address1",
+                    "address2",
+                    "zip_code",
+                )
+            },
+        ),
+    )
+
+
+admin.site.register(Address, AddressAdmin)
