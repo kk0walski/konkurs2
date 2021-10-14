7aea5761da46388b5eb14429aff3ff7e94e4b73f
kk0walski <karol.kowalski95@gmail.com>
2021-10-15 00:12:37 +0200

    Profile page without editing.

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.conf import settings
+from .models import Participant
 
 # Create your views here.
 class CurrentUserMixin(object):
@@ -18,3 +19,8 @@ class CurrentUserMixin(object):
 
 class UserDetailView(CurrentUserMixin, DetailView):
     template_name = 'account/profile.html' 
+
+    def get_context_data(self, **kwargs):
+        context =  super().get_context_data(**kwargs)
+        context['participant'] = Participant.objects.filter(user=self.get_object())
+        return context
