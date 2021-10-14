e38929ae19b0f71b8efa0206e649883a2b5f7812
kk0walski <karol.kowalski95@gmail.com>
2021-10-14 13:00:24 +0200

    Adding form fields.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,18 @@
+from django.forms.widgets import Widget
+from phonenumber_field.formfields import PhoneNumberField
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
+import datetime
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
+from .models import CustomUser, Participant
+
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('email', 'username',)
+
 
 class CustomUserChangeForm(UserChangeForm):
 
@@ -16,10 +22,24 @@ class CustomUserChangeForm(UserChangeForm):
 
 
 class SignupForm(forms.Form):
-    first_name = forms.CharField(max_length=30, label='Voornaam')
-    last_name = forms.CharField(max_length=30, label='Achternaam')
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    site = forms.URLField(required=False)
+    birthday = forms.DateField(input_formats=("%d-%m-%Y"),
+                               widget=forms.DateInput(attrs={"firstDay": 1,
+                                                             "pattern=": "\d{4}-\d{2}-\d{2}",
+                                                             "lang": "pl",
+                                                             "format": "yyyy-mm-dd",
+                                                             "type": "date",
+                                                             }), initial=datetime.date.today)
+    place_of_birth = forms.CharField(initial="Kalisz", max_length=100)
+    phone_number = PhoneNumberField(widget=PhoneNumberPrefixWidget(
+        initial="PL", attrs={'class': 'form-control'}))
+    cellphone_number = PhoneNumberField(widget=PhoneNumberPrefixWidget(
+        initial="PL", attrs={'class': 'form-control'}))
 
     def signup(self, request, user):
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.save()
+        return user
