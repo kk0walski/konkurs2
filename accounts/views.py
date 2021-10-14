9e6abbdde59eda093257371dac3579fa5748eeac
kk0walski <karol.kowalski95@gmail.com>
2021-10-14 23:08:45 +0200

    Profile page begin.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.conf import settings
 
 # Create your views here.
+class CurrentUserMixin(object):
+    model = settings.AUTH_USER_MODEL
+
+    def get_object(self, *args, **kwargs):
+        try:
+            obj = super(CurrentUserMixin, self).get_object(*args, **kwargs)
+        except AttributeError:
+            # SingleObjectMixin throws an AttributeError when no pk or slug
+            # is present on the url. In those cases, we use the current user
+            obj = self.request.user
+
+        return obj
+
+class UserDetailView(CurrentUserMixin, DetailView):
+    pass 
